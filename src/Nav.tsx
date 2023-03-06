9043ff2d8422873d562dea3acad70f2ba75d6b13
vinhanguyen <vinh.a.nguyen@gmail.com>
2023-03-06 09:13:07 -0500

    Add current job name to nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,8 +1,17 @@
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "./app/hooks";
+import { selectCurrentJob } from "./features/jobs/jobsSlice";
 
 export default function Nav() {
+  const currentJob = useAppSelector(selectCurrentJob);
+
   return (
     <ul>
+      {currentJob && (
+        <li>
+          <h2>{currentJob.name}</h2>
+        </li>
+      )}
       <li>
         <NavLink to="/">Timesheet</NavLink>
       </li>
